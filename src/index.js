0069239e9411965eabac083e8aabc5f8e0a68987
Alakesh-Bezbaruah <alakeshbezbaruahofficial@gmail.com>
2023-05-23 23:27:18 +0530

    SRA

--- src/index.js
+++ src/index.js
@@ -6,6 +6,6 @@ const app = express()
 
 app.use(express.static(path.join(__dirname +  "/public")))
 
-const PORT = process.env.PORT || 10000
+const PORT = process.env.PORT || 4000
 
 app.listen(PORT)
